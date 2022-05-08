4db092eb1a157c01105ac81c3481a17271826ca1
sheddycole <Enter your email address>
2022-05-08 06:53:32 -0400

    Feat: Get single ad

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,7 +21,22 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct)
 })
 
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+  
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+
 
 export {
-  createProduct
+  createProduct,
+  getProductById,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,13 @@ const router = express.Router()
 import {
   getProducts,
   createProduct,
+  getProductById,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router
+  .route('/:id')
+  .get(getProductById)
 
 export default router
