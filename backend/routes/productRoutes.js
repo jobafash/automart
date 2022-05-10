d8a3a20f1f95132c4b4824063003ad22e1da4e9f
jobafash <jobafash3@gmail.com>
2022-05-10 22:12:40 +0100

    complete ui

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,19 +1,19 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 import {
   getProducts,
   createProduct,
   getProductById,
   updateProduct,
-  deleteProduct
-} from '../controllers/productController.js'
-import { protect, admin } from '../middleware/authMiddleware.js'
+  deleteProduct,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route("/").get(getProducts).post(protect, createProduct);
 router
-  .route('/:id')
+  .route("/:id")
   .get(getProductById)
   .delete(protect, deleteProduct)
-  .put(protect, updateProduct)
+  .put(protect, updateProduct);
 
-export default router
+export default router;
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Welcome To Auto Mart",
+  description: "We sell the best automobiles for cheap",
+  keywords: "automobiles, buy cars, cheap cars",
+};
+
+export default Meta;
